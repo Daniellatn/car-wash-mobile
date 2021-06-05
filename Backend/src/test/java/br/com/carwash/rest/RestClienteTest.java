5a3850b11ff31580bc780876fac6c2325ac48354
Pedro Leon Paranayba Clerot <52296989+pedroleon21@users.noreply.github.com>
2021-06-05 12:46:32 -0300

    refatoração do código para adequação ao padrão maven.

---
+++ Backend/src/test/java/br/com/carwash/rest/RestClienteTest.java
@@ -0,0 +1,14 @@
+package br.com.carwash.rest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RestClienteTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
